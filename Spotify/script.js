321f47d2dc20390bcd31ee8eb0f74e1b47e0038c
PH_Marques <139821073+PH-CSS@users.noreply.github.com>
2024-03-01 17:33:35 -0300

    Add files via upload

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,1 +1,39 @@
+const button  = document.querySelectorAll(".Bot");
+const popUp = document.querySelector(".telaInteira")
+const fecharPopUp = document.querySelector(".close")
+
+
+    function openPopUp() {
+        if(popUp.style.display == "none"){
+            popUp.style.display = 'flex'
             
+        }else{
+            popUp.style.display = 'none'
+        }
+    }
+    
+    
+    button.forEach((butto) =>
+    butto.addEventListener("click", () => openPopUp())
+    )
+    
+
+
+        
+
+    fecharPopUp.addEventListener('click', () => {
+
+        if(popUp.style.display == "flex"){
+            popUp.style.display = 'none'
+            
+        }else{
+            popUp.style.display = 'flex'
+        }
+        
+    })
+
+
+
+
+       
+
